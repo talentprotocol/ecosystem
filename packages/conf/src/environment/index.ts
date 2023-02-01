dcb80de5cce1844d3b194a0989747d77169ef277
bguedes <bruno.guedes@mozantech.com>
2023-02-01 14:23:40 +0000

    feat: created foundation package - @talentprotocol/conf

---
+++ packages/conf/src/environment/index.ts
@@ -0,0 +1,10 @@
+import { EnvironmentConfInterface, ENVIRONMENT } from "./types";
+
+export const envConf: EnvironmentConfInterface =
+  process.env.ENVIRONMENT === ENVIRONMENT.DEV
+    ? {
+        LEGACY_WEB_DAPP_URL: "https://beta.talentprotocol.com",
+      }
+    : {
+        LEGACY_WEB_DAPP_URL: "https://dev.talentprotocol.com",
+      };
