9c98e081c8b36330c6da10ad161a9b5b85052ecd
bguedes <bruno.guedes@mozantech.com>
2023-02-03 10:31:21 +0000

    Revert "fix: fixed next build to use root symlinks"
    This reverts commit 6227f02430acc132d3cf9176ac927aebf2593228.

--- apps/dapp/next.config.js
+++ apps/dapp/next.config.js
@@ -1,5 +1,4 @@
 module.exports = {
   reactStrictMode: true,
-  outputFileTracingRoot: path.join(__dirname, "../.."),
   transpilePackages: ["@talentprotocol/design-system, @talentprotocol/conf"],
 };
