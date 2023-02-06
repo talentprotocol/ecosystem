55a45fcc593bcacb00de29367b482a00a0e309d3
bguedes <bruno.guedes@mozantech.com>
2023-02-06 10:39:13 +0000

    fix: removed ds button

--- apps/dapp/pages/index.tsx
+++ apps/dapp/pages/index.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@talentprotocol/design-system";
 import { envConf } from "@talentprotocol/conf";
 
 export default function Web() {
   return (
     <div>
       <p>{JSON.stringify(envConf)}</p>
-      <Button />
     </div>
   );
 }
