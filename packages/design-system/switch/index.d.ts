2a168c72b6a4d193b7134c8df7eb6f44221cd97f
helloiambguedes <bguedesdev@gmail.com>
2023-02-14 14:47:01 +0000

    removed build files

--- packages/design-system/switch/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import { Props } from "./types";
-export declare const Switch: ({
-  isChecked,
-  onChange,
-  state,
-  isDarkTheme,
-}: Props) => JSX.Element;
-//# sourceMappingURL=index.d.ts.map
