0453d50e6bcf9557a79b8d905521619538b77c09
helloiambguedes <bguedesdev@gmail.com>
2023-07-13 16:29:34 +0100

    talent-connect fix attempt

--- apps/talent-connect/src/components/card/index.tsx
+++ apps/talent-connect/src/components/card/index.tsx
@@ -46,7 +46,7 @@ export const Card = () => {
               color="primary03"
             >
               <DescriptionContainer>
-                It is a long established fact that a reader will be distracted
+                {`It is a long established fact that a reader will be distracted
                 by the readable content of a page when looking at its layout.
                 The point of using Lorem Ipsum is that it has a more-or-less
                 normal distribution of letters, as opposed to using 'Content
@@ -55,7 +55,7 @@ export const Card = () => {
                 Ipsum as their default model text, and a search for 'lorem
                 ipsum' will uncover many web sites still in their infancy.
                 Various versions have evolved over the years, sometimes by
-                accident, sometimes on purpose (injected humour and the like).
+                accident, sometimes on purpose (injected humour and the like).`}
               </DescriptionContainer>
             </Typography>
             <Input placeholder="I can help you..." />
